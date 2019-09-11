4c2a2dc10c177df8652bc6cce797107632c1452d
Luciano Ratamero <luciano@ratamero.com>
2019-09-11 20:39:11 -0300

    Export all needed functions from 'react-redux-api-tools'

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 import eventsMiddleware from './node_modules/react-redux-api-tools/lib/middleware';
+import fetchFromApi from './node_modules/react-redux-api-tools/lib/api';
 import { Event, RequestEvent, getCurrentStateFromEvent, dispatchEvent } from './lib/events';
 
-export { eventsMiddleware, Event, RequestEvent, getCurrentStateFromEvent, dispatchEvent };
+export {
+  eventsMiddleware,
+  Event,
+  RequestEvent,
+  getCurrentStateFromEvent,
+  dispatchEvent,
+  fetchFromApi,
+};
