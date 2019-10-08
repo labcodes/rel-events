8785a1b01ef9375ad718da7086e55b07ca3a7634
Luciano Ratamero <luciano@ratamero.com>
2019-10-08 11:49:32 -0300

    Add tests for combineEventReducers and _combineConflictingReducers

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,4 +1,11 @@
-import { dispatchEvent, getCurrentStateFromEvent } from '../lib/helpers';
+import {
+  dispatchEvent,
+  getCurrentStateFromEvent,
+  combineEventReducers,
+  _combineConflictingReducers,
+  // eslint-disable-next-line import/named
+  __RewireAPI__,
+} from '../lib/helpers';
 
 describe('getCurrentStateFromEvent', () => {
   it('should throw if we do not pass an event', async () => {
@@ -53,5 +60,101 @@ describe('dispatchEvent', () => {
 
     expect(event.toRedux).toHaveBeenCalledWith({ test: 'data' });
     expect(store.dispatch).toHaveBeenCalledWith(event.toRedux({ test: 'data' }));
+  });
+});
+
+describe('combineEventReducers', () => {
+  it('should call return empty object if passed nothing', async () => {
+    expect(combineEventReducers()).toEqual({});
+    expect(combineEventReducers([])).toEqual({});
+  });
+
+  it('should return object with reducers if passed events', async () => {
+    const dummyEvents = [
+      { name: 'event1', createReducers: () => ({ event1: 'called1' }) },
+      { name: 'event2', createReducers: () => ({ event2: 'called2' }) },
+    ];
+    expect(combineEventReducers(dummyEvents)).toEqual({
+      event1: 'called1',
+      event2: 'called2',
+    });
+  });
+
+  it('should throw if passed `useDataFrom` key pointing at unexisting event', async () => {
+    const dummyEvents = [
+      { name: 'event1', createReducers: () => ({ event1: 'called1' }) },
+      {
+        name: 'event2',
+        useDataFrom: 'unexistingEvent',
+        createReducers: () => ({ event2: 'called2' }),
+      },
+    ];
+    expect(() => combineEventReducers(dummyEvents)).toThrow(
+      'Event with unexistingEvent name not found.',
+    );
+  });
+
+  it('should call _combineConflictingReducers if `useDataFrom` key exists in an event', async () => {
+    const dummyEvents = [
+      { name: 'event1', createReducers: () => ({ event1: 'called1' }) },
+      {
+        name: 'event2',
+        useDataFrom: 'event1',
+        createReducers: () => ({ event2: 'called2' }),
+      },
+      {
+        name: 'event3',
+        useDataFrom: 'event1',
+        createReducers: () => ({ event3: 'called3' }),
+      },
+    ];
+
+    const mockedCombineConflictingReducers = jest.fn(() => ({ test: 'return' }));
+    __RewireAPI__.__Rewire__('_combineConflictingReducers', mockedCombineConflictingReducers);
+
+    const returnedData = combineEventReducers(dummyEvents);
+
+    expect(mockedCombineConflictingReducers).toHaveBeenCalledWith([
+      { event1: 'called1' },
+      { event2: 'called2' },
+      { event3: 'called3' },
+    ]);
+    expect(returnedData).toEqual({
+      event1: { test: 'return' },
+      event2: 'called2',
+      event3: 'called3',
+    });
+  });
+});
+
+describe('_combineConflictingReducers', () => {
+  it('returns function that returns same state if passed nothing', async () => {
+    const returnedFunction = _combineConflictingReducers();
+    expect(returnedFunction({ test: 'state' })).toEqual({ test: 'state' });
+  });
+
+  it('returns function that combines alterations of the same state', async () => {
+    const dummyReducers = [
+      {
+        event1: state => {
+          state.event1 = 'test1';
+          return state;
+        },
+      },
+      {
+        event2: state => {
+          state.event2 = 'test2';
+          return state;
+        },
+      },
+    ];
+
+    const returnedFunction = _combineConflictingReducers(dummyReducers);
+
+    expect(returnedFunction({ test: 'state' })).toEqual({
+      test: 'state',
+      event1: 'test1',
+      event2: 'test2',
+    });
   });
 });
